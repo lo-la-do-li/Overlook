73fa901903c8cd050d37ed0a6933e882157f050f
Lola Dolinsky <dolinsky.lola@gmail.com>
2020-11-03 11:23:02 -0700

    Add query selectors, eventListener, and function for user login

--- src/index.js
+++ src/index.js
@@ -2,9 +2,33 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
+// import './css/base.scss';
+import './css/styles.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// console.log('This is the JavaScript entry file - your code begins here.');
+
+const loginForm = document.getElementById('login-form');
+const loginButton = document.getElementById('login-form-submit');
+const loginErrorMsg = document.getElementById('login-error-msg');
+
+loginButton.addEventListener('click', grantAccess);
+
+function grantAccess(event) {
+  event.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+
+  if (username === 'manager' && password === 'overlook2020') {
+    alert('You have successfully logged in as a manager.');
+    // location.reload();
+  }
+  else if (username === `customer${customer.id}` && password === 'overlook2020') {
+    alert('You have successfully logged in as a customer.');
+    // location.reload();
+  } else {
+    loginErrorMsg.style.opacity = 1;
+  }
+}
