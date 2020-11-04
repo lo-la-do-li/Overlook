62d8e57d47a9ac03bffa651f4df8927b958a15e3
Lola Dolinsky <dolinsky.lola@gmail.com>
2020-11-03 22:45:28 -0700

    Add apiCalls get and post data properties with callback funcs

---
+++ src/apiCalls.js
@@ -0,0 +1,37 @@
+const getData = (path) => {
+return fetch(path)
+  .then(response => response.json())
+  .catch(err => console.log(err))
+}
+
+const addData = (path, data) => {
+return fetch(path, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(data)
+})
+  .then(response => response.json())
+  .catch(err => console.log(err))
+}
+const apiCalls = {
+
+  getUserData: () => {
+    return getData('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+  },
+
+  getRoomData: () => {
+    return getData('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+  },
+
+  getBookingData: () => {
+    return getData('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+  },
+
+  addBookingData: (bookingData) => {
+    return addData('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings', bookingData)
+  }
+}
+
+export default apiCalls;
